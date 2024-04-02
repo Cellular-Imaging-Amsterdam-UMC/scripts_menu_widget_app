1f1e1bf1013da842722f9e7a33bb71fde7735fd9
rodrigolearns <rodrigo.rosas3@gmail.com>
2024-04-02 16:07:15 +0200

    name editing

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='scripts-widget-webapp',
+    version='0.0.1',
+    description="Widget to display the scripts menu in OMERO.web",
+    packages=find_packages(),
+    keywords=['omero', 'example', 'react', 'scritpts', 'menu', 'widget'],
+)
