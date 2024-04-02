c8c7be35d43360934be92403c8f7d7c805540be6
rodrigolearns <rodrigo.rosas3@gmail.com>
2024-04-02 16:09:41 +0200

    more name editing to scritpts_menu_widget_app

--- deploy_build.sh
+++ deploy_build.sh
@@ -2,8 +2,8 @@
 
 #copy over html,css,js and templates
 echo "Deploying built resources..."
-mkdir -p react_webapp/templates/react_webapp/
-mkdir -p react_webapp/static/react_webapp/
+mkdir -p scritpts_menu_widget_app/templates/scritpts_menu_widget_app/
+mkdir -p scritpts_menu_widget_app/static/scritpts_menu_widget_app/
 
-cp build/index.html react_webapp/templates/react_webapp/
-cp -r build/static/* react_webapp/static/react_webapp
+cp build/index.html scritpts_menu_widget_app/templates/scritpts_menu_widget_app/
+cp -r build/static/* scritpts_menu_widget_app/static/scritpts_menu_widget_app
